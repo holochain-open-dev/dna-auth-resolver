8cc4635fca502e067e3e62883379c4fffb09d70c
pospi <pospi@spadgos.com>
2022-06-08 21:42:03 +1000

    update Nix env to latest Holochain vers

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
-  holonixRev = "52158409f9b76b442e592e8f06632b0e57a6c365";
+  holonixRev = "c7a0b2c75480d429d570c94909cec3210280ad4c";
 
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_127";
+    holochainVersionId = "v0_0_143";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
@@ -12,5 +12,6 @@ in nixpkgs.mkShell {
     # :TODO: binaryen, wasm-opt?
     # Additional packages go here
     nodejs-16_x
+    nodePackages.pnpm
   ];
 }
